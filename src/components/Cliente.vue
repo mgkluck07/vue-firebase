ec2e146df0cac1adb88d058526b97f42da28ad82
mkluck <mkluck@abzcomunicacion.com>
2019-02-24 16:09:37 -0300

    Instalacion Vuetify

---
+++ src/components/Cliente.vue
@@ -0,0 +1,52 @@
+<template>
+    <div class="seccion">
+        <div class="atributo">
+            <span>{{ nombreCompleto }}</span>
+        </div>
+        <div class="atributo">
+            <img :src="fotoPerfil" alt="">
+        </div>
+        <div class="atributo">
+            <a :href="biografia" target="_blank">Biografia</a>
+        </div>
+        <div class="atributo">
+            <span>Edad: {{ edad }}</span>
+        </div>
+        <div class="atributo">
+            <span>{{ email }}</span>
+        </div>
+    </div>
+</template>
+
+<script>
+export default {
+    props: ['email'],
+    data() {
+        return {
+            nombre: 'Isaac',
+            apellido: 'Newton',
+            fechaNacimiento: new Date(1642, 12, 25),
+            fotoPerfil: 'https://upload.wikimedia.org/wikipedia/commons/thumb/8/83/Sir_Isaac_Newton_%281643-1727%29.jpg/220px-Sir_Isaac_Newton_%281643-1727%29.jpg',
+            biografia: 'https://es.wikipedia.org/wiki/Isaac_Newton'
+        }
+    },
+    computed: {
+        nombreCompleto() {
+            return `${this.nombre} ${this.apellido}`;
+        },
+        edad() {
+            return this.calcularEdad(this.fechaNacimiento);
+        }
+    },
+    methods: {
+        calcularEdad(fecha){
+            let fechaActual = new Date();
+            let diferencia = fechaActual - fecha;
+            return Math.floor(diferencia / (1000 * 60 * 60 * 24 * 365.25));
+        }
+    },
+    created() {
+        this.$emit('onCambioEmail','newton@email.com');
+    },
+}
+</script>
---
+++ src/components/Email.vue
@@ -0,0 +1,33 @@
+<template>
+    <div class="seccion">
+        <div class="atributo">
+            <input v-model="email" @keyup.enter="actualizarEmail" type="text">
+        </div>
+    </div>
+</template>
+
+<script>
+export default {
+    props: [ 'emailRecibido' ],
+    data() {
+        return {
+            email: ''    
+        }
+    },
+    methods: {
+        actualizarEmail(event){
+            this.$emit('onCambioEmail',this.email);
+        }
+    },
+    watch: {
+        emailRecibido(){
+            this.email = this.emailRecibido;
+        }
+    },
+}
+</script>
+<style>
+.email {
+    text-align: left;
+}
+</style>
